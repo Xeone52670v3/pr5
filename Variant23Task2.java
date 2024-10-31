8e78c104255c524a5c0fa128ef2426cf7f2dc518
Xeone52670v3 <152479320+Xeone52670v3@users.noreply.github.com>
2024-10-31 20:05:54 +0200

    Update Variant23Task2.java

--- Variant23Task2.java
+++ Variant23Task2.java
@@ -6,7 +6,18 @@ public class Variant23Task2 {
         double a = scanner.nextDouble();
         System.out.print("Enter the value of b: ");
         double b = scanner.nextDouble();
-        double f_x = Math.pow(a, 2) - Math.pow(b, 2) + 2 * a * b;
+        System.out.print("Enter the value of x: ");
+        double x = scanner.nextDouble();
+        double f_x;
+        if (x < 9) {
+            f_x = (a * x + 7) / (5 * Math.sqrt(b * x + 1));
+        } else if (x == 15) {
+            f_x = Math.sin(x);
+        } else {
+            System.out.println("f(x) is not defined for the given x.");
+            scanner.close();
+            return;
+        }
         System.out.println("Calculated f(x): " + f_x);
         scanner.close();
     }
