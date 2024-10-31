7690f8d06a0b806f16ccc49a20d3d5a4a9dc4433
Xeone52670v3 <152479320+Xeone52670v3@users.noreply.github.com>
2024-10-31 20:05:08 +0200

    Update Variant23.java

--- Variant23.java
+++ Variant23.java
@@ -4,8 +4,8 @@ public class Variant23 {
         double b = 0.6;
         double c = 2.4 * Math.pow(10, -4);
         double t = 15;
-        double x = Math.pow(lambda, 2) + Math.sqrt(b) - Math.log(t + c);
-        double y = Math.sin(b * t) + Math.exp(lambda * c);
+        double x = Math.exp(-lambda * b) * Math.cos(b);
+        double y = b * Math.cos(t * Math.sqrt(c + b));
         System.out.println("Calculated x: " + x);
         System.out.println("Calculated y: " + y);
     }
